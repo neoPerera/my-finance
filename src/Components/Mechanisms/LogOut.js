7259454a90d04dac2d985e8c6e9df56c163680f7
Chanuth Nilushan <chanuthnilushan@gmail.com>
2024-02-07 11:44:59 +0530

    Update LogOut.js
    Fixed loading glitch issue. currently added set time out to simulate async. Need to call logout api call to do the other processing

--- src/Components/Mechanisms/LogOut.js
+++ src/Components/Mechanisms/LogOut.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import Loading from "../Elements/Loading";
+
+
 function LogOut() {
+  const [loading, setLoading] = useState(true);
 
-    console.log("Logged Out");
-    
-    // Read JWT token from storage
-    localStorage.removeItem('jwt_token');
+  useEffect(() => {
+    // Simulate asynchronous logout process
+    const logoutProcess = async () => {
+      console.log("Logging Out...");
 
-    return(<Navigate to="/login" />);
+      // Simulate some asynchronous task (e.g., API request, timeout)
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      // Perform actual logout actions
+      console.log("Logged Out");
+
+      // Read JWT token from storage
+      localStorage.removeItem("jwt_token");
+
+      // Update loading state to false
+      setLoading(false);
+    };
+
+    logoutProcess();
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  return loading ? <Loading /> : <Navigate to="/login" />;
 }
 
 export default LogOut;
