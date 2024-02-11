ce4ef752545185bd3bf6b5945b81b81f0ef400ce
Chanuth Nilushan <chanuthnilushan@gmail.com>
2024-02-11 23:03:02 +0530

    Update SideBar-ant.js
    Removed the axios get request from this component. and added it to Home component

--- src/Components/Elements/SideBar-ant.js
+++ src/Components/Elements/SideBar-ant.js
@@ -8,85 +8,9 @@ import {
   FastForwardOutlined
 } from "@ant-design/icons";
 import { Layout, Menu, theme } from "antd";
-import Axios from "axios";
 const { Sider } = Layout;
-const SideBarAnt = ({ collapsed, MenuItemClicked }) => {
-  const [responseData, setResponseData] = useState([]);
-  const [formData, setFormData] = useState([]);
-  const [spinning, setSpinning] = React.useState(false);
-  const iconComponents = {
-    BookOutlined,
-    DashboardOutlined,
-    LogoutOutlined,
-    TransactionOutlined,
-    DeliveredProcedureOutlined,
-    FastForwardOutlined
-    // Add more components as needed
-  };
-
-  useEffect(() => {
-    const getForms = async () => {
-      console.log("Getting Exp Seq");
-
-      try {
-        const response = await Axios.get(
-          `${process.env.REACT_APP_API_URL}api/home/getForms`
-        );
-
-        // Assuming the fetched data is an array
-
-        setResponseData(response.data);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    getForms();
-  }, []);
-
-  useEffect(() => {
-    if (!responseData) {
-      // Handle case where responseData is undefined
-      return;
-    }
-
-    // Declare formattedData outside of the map function
-    const formattedData = [];
-
-    responseData.forEach((item, index) => {
-      const formattedItem = {
-        key: item.str_form_id,
-        icon: item.str_icon
-          ? React.createElement(iconComponents[item.str_icon])
-          : null,
-        label: item.str_form_name,
-        link: item.str_link,
-        // children: [],
-      };
-
-      if (item.str_menu_id) {
-        const parent = formattedData.find(
-          (parentItem) => parentItem.label === item.str_menu_id
-        );
-
-        if (parent) {
-          parent.children.push(formattedItem);
-        } else {
-          const newParent = {
-            key: index.toString(),
-            label: item.str_menu_id,
-            children: [formattedItem],
-          };
-          formattedData.push(newParent);
-        }
-      } else {
-        formattedData.push(formattedItem);
-      }
-    });
-
-    // Update the state with the formatted data
-    setFormData(formattedData);
-  }, [responseData]);
+const SideBarAnt = ({ collapsed, MenuItemClicked,sideBarFormData }) => {
+  
 
   return (
     <Sider
@@ -104,7 +28,7 @@ const SideBarAnt = ({ collapsed, MenuItemClicked }) => {
         mode="inline"
         // defaultSelectedKeys={["1"]}
         onClick={MenuItemClicked}
-        items={formData}
+        items={sideBarFormData}
       />
     </Sider>
   );
