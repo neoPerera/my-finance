26c85d5583345f96176e62ddc60624641da6b88b
Chanuth Perera <chanuthnilushan@gmail.com>
2025-05-19 21:01:25 +0530

    Break Dashboard controller to multple get requests

--- src/Components/Pages/SubPages/DashBoard.js
+++ src/Components/Pages/SubPages/DashBoard.js
@@ -1,119 +1,31 @@
+import React from "react";
 import {
-  Card,
-  Col,
   Flex,
-  Row,
-  Skeleton,
-  Space,
-  Spin,
-  Statistic,
-  Table,
   message,
 } from "antd";
-import * as React from "react";
-import { Liquid, Pie, Sankey } from "@antv/g2plot";
-import { useState, useEffect, useRef } from "react";
-import CountUp from "react-countup";
-import Axios from "axios";
 import DashboardAccountBalance from "./Dashboards/DashboardAccountBalance";
 import DashboardAccountIncome from "./Dashboards/DashboardAccountIncome";
 import DashboardAccountExpense from "./Dashboards/DashboardAccountExpense";
 import DashboardTransactionsTable from "./Dashboards/DashboardTransactionsTable";
 
 function DashBoard() {
-  // Use React state to manage the chart instance
 
-  const [spinning, setSpinning] = React.useState(null);
   const [messageApi, contextHolder] = message.useMessage();
-  const [chartData, setChartData] = React.useState({
-    chart1: [],
-    chart2: [],
-    chart3: [],
-    chart4: [[], 0],
-    chartExpenses: [],
-    chartIncomes: []
-  });
-  const formatter = (value) => <CountUp end={value} separator="," />;
-  const data = {
-    transTable: {
-      columns: [
-        // {
-        //   title: "Key",
-        //   dataIndex: "key",
-        //   key: "Key",
-        // },
-        {
-          title: "Account",
-          dataIndex: "account",
-          key: "account",
-        },
-        {
-          title: "Reason",
-          dataIndex: "name",
-          key: "age",
-        },
-        {
-          title: "Amount",
-          dataIndex: "int_amount_char",
-          key: "amount",
-        },
-      ],
-    },
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setSpinning(true);
-      try {
-        console.log(window.env?.REACT_APP_API_URL);
-        const response = await Axios.get(
-          `${window.env?.REACT_APP_API_URL}api/dashboard`
-        );
-
-        console.log("income List Data:", response.data);
-
-        const newData = response.data;
-
-        // Update the state with the new data
-        setChartData({
-          chart1: newData.chart1[0] || [],
-          chart2: newData.chart2[0] || [],
-          chart3: newData.chart3[0] || [],
-          chart4: newData.chart4 || [],
-          chartExpenses: newData.chartExpenses[0] || [],
-          chartIncomes: newData.chartIncomes[0] || []
-        });
-        console.log(newData);
-        setSpinning(false);
-      } catch (error) {
-        setSpinning(false);
-        messageApi.open({
-          type: "error",
-          content: `${error}`,
-        });
-        console.error("Error fetching income list:", error);
-      }
-    };
-    fetchData();
-  }, []);
+  
 
   return (
     <>
-      {spinning ? (
-        <Skeleton active />
-      ) : (
-        <>
-          {contextHolder}
-          <Flex wrap="wrap" gap="middle">
-            {/* <Row> */}
-            <DashboardAccountBalance chartData={chartData} formatter={formatter} />
-            <DashboardAccountIncome chartData={chartData} formatter={formatter} />
-            <DashboardAccountExpense chartData={chartData} formatter={formatter} />
-            <DashboardTransactionsTable chartData={chartData} data={data} />
-          </Flex>
-        </>
-      )}
+
+      {contextHolder}
+      <Flex wrap="wrap" gap="middle">
+        {/* <Row> */}
+        <DashboardAccountBalance />
+        <DashboardAccountIncome />
+        <DashboardAccountExpense />
+        <DashboardTransactionsTable />
+      </Flex>
     </>
+
   );
 }
 
--- src/Components/Pages/SubPages/Dashboards/DashboardTransactionsTable.js
+++ src/Components/Pages/SubPages/Dashboards/DashboardTransactionsTable.js
@@ -1,23 +1,91 @@
 import React from 'react';
-import { Col, Card, Table } from 'antd';
+import { Col, Card, Table, message, Skeleton } from 'antd';
+import { useState, useEffect, useRef } from "react";
+import Axios from "axios";
+import CountUp from "react-countup";
 
-const DashboardTransactionsTable = ({ chartData, data }) => {
-  const transactions = chartData?.chart4?.[0] || [];
+const DashboardTransactionsTable = () => {
+  const [spinning, setSpinning] = React.useState(null);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [chartData, setChartData] = React.useState([]);
+  const data = {
+    transTable: {
+      columns: [
+        // {
+        //   title: "Key",
+        //   dataIndex: "key",
+        //   key: "Key",
+        // },
+        {
+          title: "Account",
+          dataIndex: "account",
+          key: "account",
+        },
+        {
+          title: "Reason",
+          dataIndex: "name",
+          key: "age",
+        },
+        {
+          title: "Amount",
+          dataIndex: "int_amount_char",
+          key: "amount",
+        },
+      ],
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setSpinning(true);
+      try {
+        console.log(window.env?.REACT_APP_API_URL);
+        const response = await Axios.get(
+          `${window.env?.REACT_APP_API_URL}api/dashboard/getdashboardtransactions`
+        );
+
+        console.log("income List Data:", response.data);
+
+        const newData = response.data;
+
+        // Update the state with the new data
+        setChartData(newData || []);
+        console.log(newData);
+        setSpinning(false);
+      } catch (error) {
+        setSpinning(false);
+        messageApi.open({
+          type: "error",
+          content: `${error}`,
+        });
+        console.error("Error fetching income list:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <Col xs={20} sm={12} md={8} lg={6} xl={7}>
-      <Card title="Transactions">
-        {transactions.length > 0 && (
-          <Table
-            size="small"
-            scroll={{ x: 500 }}
-            dataSource={transactions}
-            columns={data?.transTable?.columns || []}
-            pagination={{ pageSize: 10 }}
-          />
-        )}
-      </Card>
-    </Col>
+    <>
+      {spinning ? (
+        <Skeleton.Node active />
+      ) : (
+        <>
+          <Col xs={20} sm={12} md={8} lg={6} xl={7}>
+            <Card title="Transactions">
+              {chartData.length > 0 && (
+                <Table
+                  size="small"
+                  scroll={{ x: 500 }}
+                  dataSource={chartData}
+                  columns={data?.transTable?.columns || []}
+                  pagination={{ pageSize: 10 }}
+                />
+              )}
+            </Card>
+          </Col>
+        </>
+      )}
+    </>
   );
 };
 
